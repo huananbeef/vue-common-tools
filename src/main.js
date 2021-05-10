64393ad1e94e8d2a5b52d9099f37e75a1efbed2d
renzhongyu <renzhongyu@sinosoft.com.cn>
2021-05-10 18:44:14 +0800

    fix bug

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router.js';
+import store from './store/index';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+import 'swiper/swiper-bundle.css'
+Vue.use(VueAwesomeSwiper);
+Vue.use({
+  router,
+  store
+})
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/router.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+// const Layout = () => import(/* webpackChunkName: "user" */)
+Vue.use(Router);
